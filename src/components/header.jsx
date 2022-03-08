2809090465961f62057eba5bb88eb0b5922b7709
Bastián <bastian.p@outlook.com>
2022-03-08 12:23:09 -0300

    First commit

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+    
+    render() {
+        return (
+            <header class="header">
+                <img class="header-logo" src="images/logo.png"/>
+                <h1 class="header-tittle">Imagen a PDF</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header.jsx';
+import Main from './components/main.jsx';
+import Footer from './components/footer.jsx';
+
+
+import './styles/normalize.css';
+import './styles/main.css';
+
+
+ReactDOM.render(
+    <React.StrictMode>
+    <Header />
+    <Main />
+    <Footer />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
