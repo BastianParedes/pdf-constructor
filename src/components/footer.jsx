b4e447bf1660a3468045479f5b4040fbf1298f94
Bastián <bastian.p@outlook.com>
2022-03-12 20:39:35 -0300

    Quitados import React innecesarios

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
-class Footer extends React.Component {
-    render() {
-        return (
-            <footer className="footer">
-                <p className="p-footer">Website designed by @Bastián Paredes, © 2022</p>
-            </footer>
-        );
-    };
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="p-footer">Website designed by @Bastián Paredes, © 2022</p>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="header">
-                <img className="header-logo" alt='Bastián Paredes' src={require("../images/logo.png")}/>
-                <h1 className="header-tittle">Imagen a PDF</h1>
-            </header>
-        );
-    };
+function Header() {
+    return (
+        <header className="header">
+            <img className="header-logo" alt='Bastián Paredes' src={require("../images/logo.png")}/>
+            <h1 className="header-tittle">Imagen a PDF</h1>
+        </header>
+    );
 }
 
 export default Header;
